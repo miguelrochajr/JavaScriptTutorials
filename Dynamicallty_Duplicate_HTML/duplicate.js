468f420d31bce3245471f0b58853fec65903dd5e
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2017-10-19 04:40:18 -0300

    Duplicate HTML on a click added

---
+++ Dynamicallty_Duplicate_HTML/duplicate.js
@@ -0,0 +1,12 @@
+// Source code:
+// https://stackoverflow.com/a/4427135
+
+var i = 0;
+var original = document.getElementById('duplicater');
+
+function duplicate() {
+    var clone = original.cloneNode(true); // "deep" clone
+    clone.id = "duplicater" + ++i;
+    // or clone.id = ""; if the divs don't need an ID
+    original.parentNode.appendChild(clone);
+}
